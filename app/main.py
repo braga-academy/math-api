9c73de7793672d70c78bade1dcf1ceab658e37e8
lucianobragaweb <lucianobragaweb@gmail.com>
2025-03-10 07:50:42 -0300

    add wellcome route

--- app/main.py
+++ app/main.py
@@ -16,5 +16,15 @@ redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
 
 cache = RedisCache(endpoint="localhost", port=6379, namespace="fastapi-cache", serializer=JsonSerializer())
 
+@app.get("/")
+async def root():
+    return {
+        "message": "Bem-vindo à Calc API!",
+        "documentation": {
+            "swagger": "http://localhost:8000/docs",
+            "redoc": "http://localhost:8000/redoc"
+        }
+    }
+
 app.include_router(math_router, prefix="/api/v1")
 app.include_router(health_router, prefix="/api/v1")
